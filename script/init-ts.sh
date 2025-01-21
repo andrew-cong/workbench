91db592059c49e3037d269fd7974216f17c9754c
Andrew <andrew@Andrews-MacBook-Air.local>
2025-01-21 12:23:33 +0800

    init

---
+++ script/init-ts.sh
@@ -0,0 +1,13 @@
+npm init -y
+npm install typescript --save-dev
+npm install ts-node @types/node --save-dev
+
+npx tsc --init
+cp ~/code/workbench/config/tsconfig.json .
+
+mkdir src
+touch src/index.ts
+echo 'const message: string = "Hello, TypeScript!";console.log(message);' >> src/index.ts
+
+npx tsc
+node dist/index.js
